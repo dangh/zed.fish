a6277b22bfae9afb2303688214e038d1c25f7f5d
Dang <huynhminhdang@gmail.com>
2024-04-11 05:51:13 +0800

    unset non-zed vars

--- conf.d/zed.fish
+++ conf.d/zed.fish
@@ -3,27 +3,31 @@ set -q ZED_TERM || exit
 
 set -q ZED_TERM_WRAPPED || begin
     # uninherit variables
-    set -l vars
+    set -l all_vars
+    set -l zed_vars TERM SHELL
+    set -l non_zed_vars
     set -l env_cmd
     set -S | while read -L line
         if string match -qr '^\$(?<var>\w+)\[\d+\]: \|(?<val>.*?)\|$' -- $line
-            if string match -irq '(^|\W|_)zed(_|\W|$)' -- $var $val
-                set -a vars $var
+            if not contains $var $all_vars
+                set -a all_vars $var
+            end
+            if string match -rq '(^|\W|_)ZED(_|\W|$)' -- $var || string match -rq '(^|\W|_)Zed(_|\W|$)' -- $val
+                if not contains $var $zed_vars
+                    set -a zed_vars $var
+                end
             end
         end
     end
-    for var in $vars
-        set -l set_cmd
-        set -l val $$var
-        for i in (seq (count $val))
-            set -a set_cmd \""$val[$i]\""
+    for var in $all_vars
+        if not contains $var $zed_vars
+            set -a non_zed_vars $var
         end
-        if set -qx $var
-            set -p set_cmd set -gx $var
-        else
-            set -p set_cmd set -g $var
-        end
-        set -a env_cmd "$set_cmd;"
     end
-    exec env -i TERM=$TERM SHELL=$SHELL ZED_TERM_WRAPPED=1 $SHELL -il -C "$env_cmd"
+    set -l exec_cmd ZED_TERM_WRAPPED=1 exec env
+    for var in $non_zed_vars
+        set -a exec_cmd "-u $var"
+    end
+    set -a exec_cmd $SHELL -il
+    eval $exec_cmd
 end
